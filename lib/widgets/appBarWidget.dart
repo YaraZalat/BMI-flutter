649e81e7076e98e98876c57a053a77e4ae45fec3
YaraZalat <yarazalat6@gmail.com>
2025-07-09 12:33:19 +0300

    Add custom fixed size AppBar widget

---
+++ lib/widgets/appBarWidget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+ class Appbarwidget extends StatelessWidget implements PreferredSizeWidget{
+   final String title;
+   const Appbarwidget({super.key, required this.title});
+   @override
+   Widget build(BuildContext context) {
+     return AppBar(
+       centerTitle: true,
+       backgroundColor: Color(0xff24263B),
+       title: Text(title,style: TextStyle(fontSize: 20,color: Colors.white,fontWeight: FontWeight.w500),textAlign: TextAlign.center,),
+     );
+   }
+  Size get preferredSize => Size.fromHeight(40);
+ }
+
